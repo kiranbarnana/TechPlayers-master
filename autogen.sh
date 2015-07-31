b56c69ec10a4032cc8628aa1907c825dcca9ff16
TechPlayers <kiran.barnana@verizon.com>
2015-07-31 06:18:45 -0700

    master
    master

---
+++ autogen.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+autoreconf --force --install
+cd Techplayers-master
+mkdir lib
+pwd
+ls
+sh ./configure
+make
+make check
+make install DESTDIR=~/kiranbarnana/TechPlayers/TechPlayers-master/lib/
+make clean
